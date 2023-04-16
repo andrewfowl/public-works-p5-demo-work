22c64e1732448404370baa624fa1383869a6f9b0
andrewfowl <andrewfowl@mail.ru>
2023-04-15 20:24:26 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import * as p5 from 'p5';
 import {generateTraits} from "./traits";
 
-let devMode = true;
+let devMode = false;
 if(devMode){
     /**
      * Enable new hashes on click during development.
@@ -23,18 +23,22 @@ let s = (sk) => {
     const {traits, attributes} = generateTraits(createPrng());
 
     setProperties(attributes, traits);
-    let sCol;
+    let sCol; 
+    let Counter=1;
 
     sk.setup = () => {
         const dimensions = getDimensions();
-        sk.createCanvas(...dimensions)
-        sk.colorMode(sk.HSL)
+        const canvas = sk.createCanvas(...dimensions);
+        canvas.attribute('thought', '');
+        sk.colorMode(sk.HSL);
         sk.noLoop();
     }
 
     sk.draw = () => {
         sk.background(255)
         let g = 50;
+        let skipTwo = (traits.numCircles === 'Low') ? 0 : 1;
+        let skipOne = (traits.numCircles === 'Low' || traits.numCircles === 'Medium') ? 0 : 1;
         const prng = createPrng();
         for (let x = 0; x <= sk.width ; x += g) {
           for (let y = 0; y <= sk.width; y += g) {
@@ -43,10 +47,19 @@ let s = (sk) => {
             let j = g / Math.floor(prng.randomInt(1, 20)) ;
             for (let sx = -g + j ; sx <= g - j ; sx += j) {
               for (let sy = -g+ j ; sy <= g - j ; sy += j) {
+                Counter = (Counter === 3) ? 1 : ((Counter === 2) ? 3 : 1);
+                const depict = () => {
                 sCol = (prng.randomInt(0, 2)===0) ? traits.aCol : ((prng.randomInt(0, 2)===2) ? traits.bCol : traits.cCol);
                 sk.stroke(sCol.hue, sCol.saturation, sCol.lightness);
                 sk.strokeWeight(prng.randomInt(1, j));
-                sk.point(sx + prng.randomInt(-g, g), sy + prng.randomInt(-g, g));
+                sk.point(sx + prng.randomInt(-g, g), sy + prng.randomInt(-g, g));}
+                if (Counter === 1) {
+                    depict();
+                  } else if (Counter === 2 && skipTwo === 0) {
+                    depict();
+                  } else if (Counter === 3 && skipOne === 0 && skipTwo === 0) {
+                    depict();
+                  }
               }
             }
             sk.pop();
